a09cd1f0f63283659cf777199c36876b83199706
Stefan Foulis <stefan@foulis.ch>
2022-01-14 23:54:14 +0000

    update flashall script

--- flashall.sh
+++ flashall.sh
@@ -6,24 +6,24 @@ pushd ~/klipper
 
 # === Duet2 Maestro MCU ===
 export CFG=maestro
-export DEV=/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0
+export DEV=/dev/serial/by-id/usb-Klipper_sam4s8c_00313853314E57313235303135303538-if00
+# export DEV=/dev/ttyACM0
 echo "[${CFG}] compiling"
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ clean
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ all
 echo "[${CFG}] flashing"
-${PWD}/scripts/flash-sdcard.sh -f ${PWD}/out-${CFG}/klipper.bin ${DEV} btt-skr-turbo-v1.4
-
+make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ FLASH_DEVICE=${DEV} flash
 
 
 # === Arduino Nano MCU ===
-# export CFG=arduinonano
-# export DEV=/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0
+export CFG=arduinonano
+export DEV=/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0
 # export DEV=/dev/ttyUSB0
-# echo "[${CFG}] compiling"
-# make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ clean
-# make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ all
-# echo "[${CFG}] flashing"
-# avrdude -carduino -patmega328p -P${DEV} -b57600 -D -Uflash:w:out-${CFG}/klipper.elf.hex:i
+echo "[${CFG}] compiling"
+make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ clean
+make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ all
+echo "[${CFG}] flashing"
+avrdude -carduino -patmega328p -P${DEV} -b57600 -D -Uflash:w:out-${CFG}/klipper.elf.hex:i
 
 
 # === Pi Host MCU ===
@@ -31,8 +31,16 @@ export CFG=pi
 echo "[${CFG}] compiling"
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out/ clean
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out/ all
+echo "[${CFG}] stopping klipper_mcu"
+sudo service klipper_mcu stop
 echo "[${CFG}] flashing"
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out/ flash
+echo "[${CFG}] updating klipper_mcu startup script"
+sudo cp "${PWD}/scripts/klipper-mcu-start.sh" /etc/init.d/klipper_mcu
+sudo update-rc.d klipper_mcu defaults
+echo "[${CFG}] starting klipper_mcu"
+sudo service klipper_mcu start
+
 
 
 popd
