a525c25f603c789d122fec2485430b77dd4c1126
Stefan Foulis <stefan@foulis.ch>
2022-08-06 13:13:21 +0100

    flashall tweak: add device for maestro

--- flashall.sh
+++ flashall.sh
@@ -6,8 +6,9 @@ pushd ~/klipper
 
 # === Duet2 Maestro MCU ===
 export CFG=maestro
-# export DEV=/dev/serial/by-id/usb-Klipper_sam4s8c_00313853314E57313235303135303538-if00
-export DEV=/dev/ttyACM0
+export DEV=/dev/serial/by-id/usb-Klipper_sam4s8c_00313853314E57313235303135303538-if00
+# export DEV=/dev/ttyACM0
+# export DEV=/dev/serial/by-id/usb-03eb_6124-if00
 echo "[${CFG}] compiling"
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ clean
 make KCONFIG_CONFIG=${PWD}/.config-${CFG} OUT=out-${CFG}/ all
